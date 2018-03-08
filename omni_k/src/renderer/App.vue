4a4f8a8006daffe744630675816dc2454fc0f358
JediParallax <joanmonterre@gmail.com>
2018-03-08 08:20:04 -0300

    first commit

---
+++ omni_k/src/renderer/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <div class="main">
+      <div class="menu-icons">
+          <a href="" alt="Escanear código de barra" title="Escanear código de barra"><img src="@/assets/barcode_scanner.png" alt=""> </a> 
+          <a href="" alt="Introducir código" title="Introducir código"><img src="@/assets/manual.png" alt=""></a>  
+      </div>
+      <consulta></consulta>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import Consulta from "@/components/Consulta";
+
+export default {
+  components: {
+    Consulta
+  },
+  name: "omni_k",
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="sass">
+
+*
+  box-sizing: border-box
+  margin: 0
+  padding: 0
+
+#app, body, html
+  width: 100%
+  height: 100%
+ 
+  overflow: hidden
+  font-family: helvetica
+
+a
+  text-decoration: none
+  color: $fourth
+
+.main
+  display: flex
+  flex-direction: column 
+  background-color: $first
+  height: 100%
+
+.menu-icons
+    background-color: $second
+    display: flex
+    flex-direction: row
+    justify-content: flex-start
+    flex: 1 0 13%
+    img
+        width: 42px
+        height: auto
+        margin: 10px 0 10px 8px
+        
+</style>
---
+++ omni_k/src/renderer/components/Consulta.vue
@@ -0,0 +1,258 @@
+<template>
+    <div class="container">
+    <form class="formulario" @submit.prevent="findBarcode()">
+        <input v-model="datos.barcode" type="text" name='codigoProducto' placeholder=" Ingrese código de barra" >
+        <button type="submit">Buscar</button>
+    </form>
+    <div class="table">
+        <div class="table-row header">
+            <div class="text th_element">SKU</div>
+            <div class="text th_element">Barcode</div>
+            <div class="text th_element">Color</div>
+            <div class="text th_element">Precio</div>
+        </div>
+        <div class="table-body">
+            <div class="table-row" v-for='dato in datos' >
+            <div  class="text td_element">{{dato.sku}}</div>
+            <div class="text td_element">{{dato.barcode}}</div>
+            <div class="text td_element">{{dato.colors}}</div>
+            <div class="text td_element">{{dato.precio}}</div>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "consulta",
+  data() {
+    return {
+      datos: [
+        {
+          sku: "0161651321",
+          barcode: "816215164",
+          colors: "azul",
+          precio: "3000$"
+        },
+        {
+          sku: "0161651322",
+          barcode: "816248964",
+          colors: "verde",
+          precio: "7000$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        },
+        {
+          sku: "01616589251",
+          barcode: "818975164",
+          colors: "azul",
+          precio: "4500$"
+        },
+        {
+          sku: "0161651329",
+          barcode: "816812644",
+          colors: "morado",
+          precio: "8642$"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="sass">
+
+.container
+    display: flex
+    flex: 1 0 auto
+    flex-direction: column
+           
+.formulario
+    display: flex;
+    justify-content: space-around
+    flex: 0 0 5%
+    margin: 20px
+ 
+    input   
+        border: $borders
+        padding: 5px
+      
+    button 
+        background-color: $second
+        border-radius: 5px
+        border: 2px solid $third
+        color: $third
+        padding: 5px
+        &:hover
+          cursor: pointer
+
+.table
+    flex: 1 0 auto
+    display: flex
+    flex-direction: column
+    margin: 0 20px 20px 20px
+    height: 400px
+
+.table-body
+  overflow-y: scroll
+  flex-direction: column
+  height: 240px
+  border: $table_borders
+  border-top: 0
+
+.table-row
+  display: flex       
+  flex-direction: row               
+  flex-wrap: nowrap        
+  width: 100%
+  
+.header 
+    background-color: $second
+    font-weight: bold
+    overflow-y: scroll
+    border: $table_borders
+    border-bottom: 0
+    .th_element
+         padding: 7px 0
+         
+.text 
+  display: flex    
+  overflow: hidden
+  border: $alt_borders
+  justify-content: center
+  flex: 1 0 110px
+
+.td_element
+   padding: 2.6px 0
+</style>
